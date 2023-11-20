311fac623184101b3ccf7157319b238a424d9452
Akhambay <assyl.akhambay@gmail.com>
2023-11-20 15:57:18 +0600

    Calculate total price

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProductList, ProductDetail
+from .views import ProductList, ProductDetail, calculate_total_price
 
 urlpatterns = [
     path("<int:pk>/", ProductDetail.as_view(), name="product_detail"),
     path("", ProductList.as_view(), name="product_list"),
+    path('order/', calculate_total_price,
+         name='order'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
@@ -11,3 +13,21 @@ class ProductList(generics.ListCreateAPIView):
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+@api_view(['POST'])
+def calculate_total_price(request):
+    data = request.data
+    product_ids = data.get('product_ids', [])
+    quantities = data.get('quantities', [])
+
+    total_price = 0
+
+    for product_id, quantity in zip(product_ids, quantities):
+        try:
+            product = Product.objects.get(pk=product_id)
+            total_price += product.price * quantity
+        except Product.DoesNotExist:
+            return Response({'error': f'Product with id {product_id} not found'}, status=404)
+
+    return Response({'total_price': total_price})
